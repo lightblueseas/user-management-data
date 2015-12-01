41784e935d726eb18a051062e5b355ec7b1ae66c
astrapi69 <asterios.raptis@gmx.net>
2015-12-01 13:01:38 +0100

    javadoc extended.

--- user-management-business/src/main/java/de/alpharogroup/user/management/service/api/RecommendationsService.java
+++ user-management-business/src/main/java/de/alpharogroup/user/management/service/api/RecommendationsService.java
@@ -6,7 +6,28 @@ import de.alpharogroup.db.service.jpa.BusinessService;
 import de.alpharogroup.user.management.entities.Recommendations;
 import de.alpharogroup.user.management.entities.Users;
 
+/**
+ * The interface {@link RecommendationsService}.
+ */
 public interface RecommendationsService extends BusinessService<Recommendations, Integer> {
-	List<Recommendations> find(Users user, Users recommended,  String email);
+	
+	/**
+	 * Find all the {@link Recommendations} objects from the given parameters.
+	 *
+	 * @param user the user the recommended another user
+	 * @param recommended the recommended user
+	 * @param email the email where to send the recommendation
+	 * @return the list of found {@link Recommendations} objects.
+	 */
+	List<Recommendations> find(Users user, Users recommended, String email);
+	
+	/**
+	 * Find the {@link Recommendations} object from the given parameters.
+	 *
+	 * @param user the user
+	 * @param recommended the recommended
+	 * @param email the email
+	 * @return the found {@link Recommendations} object
+	 */
 	Recommendations findRecommendations(Users user, Users recommended,  String email);
 }
--- user-management-domain/src/main/java/de/alpharogroup/user/management/service/api/RecommendationService.java
+++ user-management-domain/src/main/java/de/alpharogroup/user/management/service/api/RecommendationService.java
@@ -1,11 +1,34 @@
 package de.alpharogroup.user.management.service.api;
 
+import java.util.List;
+
 import de.alpharogroup.service.domain.DomainService;
 import de.alpharogroup.user.management.domain.Recommendation;
+import de.alpharogroup.user.management.domain.User;
+import de.alpharogroup.user.management.entities.Recommendations;
 
 /**
  * The interface {@link RecommendationService}.
  */
 public interface RecommendationService extends DomainService<Integer, Recommendation> {
 
+	/**
+	 * Find all the {@link Recommendation} objects from the given parameters.
+	 *
+	 * @param user the user the recommended another user
+	 * @param recommended the recommended user
+	 * @param email the email where to send the recommendation
+	 * @return the list of found {@link Recommendations} objects.
+	 */
+	List<Recommendation> find(User user, User recommended, String email);
+	
+	/**
+	 * Find the {@link Recommendations} object from the given parameters.
+	 *
+	 * @param user the user
+	 * @param recommended the recommended
+	 * @param email the email
+	 * @return the found {@link Recommendations} object
+	 */
+	Recommendation findRecommendations(User user, User recommended,  String email);
 }
