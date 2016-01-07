c0b9d3a6ae36c58653d7fde1e341f191b038af9e
astrapi69 <asterios.raptis@gmx.net>
2016-01-07 14:41:32 +0100

    new method for get only male and female gender types

--- user-management-entities/src/main/java/de/alpharogroup/user/management/enums/GenderType.java
+++ user-management-entities/src/main/java/de/alpharogroup/user/management/enums/GenderType.java
@@ -11,22 +11,38 @@ public enum GenderType {
 	/** The male. */
 	MALE,
 	/** The female. */
-	FEMALE, 
+	FEMALE,
 	/** represents a company. */
 	INCORPORATION,
 	/** The undefined. */
 	UNDEFINED;
 
 	/**
-	 * Gets the all genders without the gender constant UNDEFINED.
-	 * 
+	 * Gets the all genders without the gender type {@link GenderType#UNDEFINED}.
+	 *
 	 * @return the valid gender types
 	 */
 	public static List<GenderType> getValidGenders() {
-		GenderType[] genderValues = GenderType.values();
-		List<GenderType> genders = new ArrayList<GenderType>();
+		final GenderType[] genderValues = GenderType.values();
+		final List<GenderType> genders = new ArrayList<GenderType>();
 		for (int i = 0; i < genderValues.length; i++) {
 			if (!genderValues[i].equals(GenderType.UNDEFINED)) {
+				genders.add(genderValues[i]);
+			}
+		}
+		return genders;
+	}
+
+	/**
+	 * Gets the all genders without the gender type {@link GenderType#UNDEFINED} and {@link GenderType#INCORPORATION}.
+	 *
+	 * @return the valid gender types
+	 */
+	public static List<GenderType> getHumanGenders() {
+		final GenderType[] genderValues = GenderType.values();
+		final List<GenderType> genders = new ArrayList<GenderType>();
+		for (int i = 0; i < genderValues.length; i++) {
+			if (!genderValues[i].equals(GenderType.UNDEFINED)&& !genderValues[i].equals(GenderType.INCORPORATION)) {
 				genders.add(genderValues[i]);
 			}
 		}
