bb830aac9cdce96f32a980e29ce72b44144d06cc
Asterios Raptis <asterios.raptis@gmx.net>
2016-07-16 18:28:53 +0200

    Lombokified dao classes

--- user-management-entities/src/main/java/de/alpharogroup/user/management/daos/ContactmethodsDao.java
+++ user-management-entities/src/main/java/de/alpharogroup/user/management/daos/ContactmethodsDao.java
@@ -7,26 +7,20 @@ import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 import de.alpharogroup.user.management.entities.Contactmethods;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository("contactmethodsDao")
 public class ContactmethodsDao extends JpaEntityManagerDao<Contactmethods, Integer>{
-
 	/**
-	 * 
+	 * The serialVersionUID.
 	 */
-	private static final long serialVersionUID = -3922309556657658501L;
+	private static final long serialVersionUID = 1L;
 
+	/** The entity manager. */
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
-
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 
 }
--- user-management-entities/src/main/java/de/alpharogroup/user/management/daos/RolesDao.java
+++ user-management-entities/src/main/java/de/alpharogroup/user/management/daos/RolesDao.java
@@ -7,25 +7,19 @@ import org.springframework.stereotype.Repository;
 
 import de.alpharogroup.db.dao.jpa.JpaEntityManagerDao;
 import de.alpharogroup.user.management.entities.Roles;
+import lombok.Getter;
+import lombok.Setter;
 
 @Repository("rolesDao")
 public class RolesDao extends JpaEntityManagerDao<Roles, Integer>{
-
 	/**
 	 * The serialVersionUID.
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/** The entity manager. */
 	@PersistenceContext
+	@Getter
+	@Setter
 	private EntityManager entityManager;
-
-	@Override
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-	@Override
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;		
-	}
 }
